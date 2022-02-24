0225ea37e4bb9bf5534baf260ff367470ad26de5
ozgesonmez6 <ozgesonmez_6@hotmail.com>
2022-02-24 19:59:57 +0300

    list categories

--- MvcProject/Controllers/CategoryController.cs
+++ MvcProject/Controllers/CategoryController.cs
@@ -3,15 +3,19 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using MvcProject.Models.Entity;
 
 namespace MvcProject.Controllers
 {
     public class CategoryController : Controller
     {
         // GET: Category
+
+        MvcDbStokEntities db = new MvcDbStokEntities();
         public ActionResult Index()
         {
-            return View();
+            var values = db.Categories.ToList();
+            return View(values);
         }
     }
 }
