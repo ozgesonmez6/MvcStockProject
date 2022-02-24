d8ad73e15fbdc1bb2cb4b9ae6b3ae7325488b042
ozgesonmez6 <ozgesonmez_6@hotmail.com>
2022-02-24 20:19:44 +0300

    list products

---
+++ MvcProject/Controllers/ProductController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProject.Models.Entity;
+
+namespace MvcProject.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var values = db.Products.ToList();
+            return View(values);
+        }
+    }
+}
