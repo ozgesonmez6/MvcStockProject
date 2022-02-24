bdc73d58e3b9715945fb072b8e82ea3671130211
ozgesonmez6 <ozgesonmez_6@hotmail.com>
2022-02-24 20:49:03 +0300

    list customers

---
+++ MvcProject/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MvcProject.Models.Entity;
+
+namespace MvcProject.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        MvcDbStokEntities db = new MvcDbStokEntities();
+        public ActionResult Index()
+        {
+            var values = db.Customers.ToList();
+            return View(values);
+        }
+    }
+}
