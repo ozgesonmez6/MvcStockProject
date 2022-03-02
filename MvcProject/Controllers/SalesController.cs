81696e7477979dbdc6961bbab7913621dfbb0d3d
ozgesonmez6 <ozgesonmez_6@hotmail.com>
2022-03-02 13:19:13 +0300

    Adding Sales on a Popup Modal

--- MvcProject/Controllers/SalesController.cs
+++ MvcProject/Controllers/SalesController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MvcProject.Models.Entity;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,25 @@ namespace MvcProject.Controllers
 {
     public class SalesController : Controller
     {
+        MvcDbStokEntities db = new MvcDbStokEntities();
         // GET: Sales
         public ActionResult Index()
         {
             return View();
+        }
+
+        [HttpGet]
+        public ActionResult NewSales()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult NewSales(Sales sales)
+        {
+            db.Sales.Add(sales);
+            db.SaveChanges();
+            return View("Index");
         }
     }
 }
